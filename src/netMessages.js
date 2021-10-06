8b6e23b3d3062af806daaef108b201c764bfdf74
Ephememory <leetvipertm@gmail.com>
2021-10-06 02:01:43 -0500

    initial

---
+++ src/netMessages.js
@@ -0,0 +1,13 @@
+export const NET_MESSAGE = {
+	CONNECT: "connect",
+	CONNECT_ERRROR: "connect_error",
+	ERROR: "error",
+	ROOM_EXISTS: "room_exists",
+	ROOM_ERROR: "room_error",
+	DISCONNECT: "disconnect",
+	ID: "identity",
+	TIMEOUT: "time_over",
+	MATCH_DATA: "match_data",
+	UPDATE_PLAYER_STATUS: "update_player_status"
+
+}
