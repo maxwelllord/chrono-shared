e8b353002ae62f76c76fdb58425dfd9698fe110d
Ephememory <leetvipertm@gmail.com>
2021-10-06 13:29:10 -0500

    add more net messages

--- src/net_messages.js
+++ src/net_messages.js
@@ -4,10 +4,19 @@ export const NET_MESSAGE = {
 	ERROR: "error",
 	ROOM_EXISTS: "room_exists",
 	ROOM_ERROR: "room_error",
+	ROOM_CHECK: "room_check",
 	DISCONNECT: "disconnect",
 	ID: "identity",
 	TIMEOUT: "time_over",
 	MATCH_DATA: "match_data",
-	UPDATE_PLAYER_STATUS: "update_player_status"
+	UPDATE_PLAYER_STATUS: "update_player_status",
+	BEGIN_MATCH: "begin_match",
+	UPDATE_CARDS_PLACED: "update_cards_placed",
+	GAME_OVER: "game_over",
+	INVALID_CARD_RECEIVED: "invalid_card_received",
+	NEW_TURN: "new_turn",
+	KICKED: "kicked",
+	PLAY_AGAIN: "play_again",
+	POSTMATCH_PLAYER_UPDATE: "postmatch_player_update"
 
 }
